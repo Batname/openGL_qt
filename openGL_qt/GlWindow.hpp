a8d9530ad95cbf4bba4d07555b4366e896df69be
DenisDubinin <dadubinin@gmail.com>
2016-12-18 11:11:39 +0100

    inint

---
+++ openGL_qt/GlWindow.hpp
@@ -0,0 +1,15 @@
+
+
+#ifndef GlWindow_hpp
+#define GlWindow_hpp
+
+#include <QtOpenGL/qgl.h>
+
+class GlWindow : public QGLWidget
+{
+public:
+    
+};
+
+
+#endif /* GlWindow_hpp */
--- openGL_qt/main.cpp
+++ openGL_qt/main.cpp
@@ -1,15 +1,27 @@
-//
-//  main.cpp
-//  openGL_qt
-//
-//  Created by apple on 2016-12-18.
-//  Copyright © 2016 apple. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include <QtWidgets/qapplication.h>
+
+// custom
+#include "GlWindow.hpp"
+
+int main(int argc, char * argv[]) {
     std::cout << "Hello, World!\n";
-    return 0;
+    
+    QApplication app(argc, argv);
+    
+    GlWindow glWindow;
+    glWindow.show();
+    
+    return app.exec();
 }
